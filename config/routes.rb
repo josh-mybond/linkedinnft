35477638d868dc3168692301c7b4ba5858e7b77b
Joshua theeuf <josh@Joshuas-MacBook-Pro.local>
2022-02-22 08:50:33 +1100

    Commit

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "customers#index"
   get '/all_customers', to: 'customers#all_customers'
+  post 'webhook', to: "indices#webhook"
 end
